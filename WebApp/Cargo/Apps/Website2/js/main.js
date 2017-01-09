ae3dc6dc535590da770286492cf553034bc3c9bb
eric4848 <eric_kavalec@hotmail.com>
2017-01-09 17:33:07 -0500

    avancement siteweb
    top navbar

--- WebApp/Cargo/Apps/Website2/js/main.js
+++ WebApp/Cargo/Apps/Website2/js/main.js
@@ -1,3 +1,19 @@
+// top-navbar
+$('#top-navbar li').click(function(e) {
+    $('#top-navbar li.active').removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+        $this.addClass('active');
+    }
+    e.preventDefault();
+});
+
+$('#a-cargo').click(function(e) {
+    $('#top-navbar li.active').removeClass('active');
+    e.preventDefault();
+});
+
+
 main()
 
 function main() {
