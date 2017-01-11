49ee8f6bddc4ce6639de2d48becb4fa365f57ade
eric4848 <eric_kavalec@hotmail.com>
2017-01-11 17:57:59 -0500

    website - tutorial user interface

--- WebApp/Cargo/Apps/Website2/js/main.js
+++ WebApp/Cargo/Apps/Website2/js/main.js
@@ -1,18 +1,21 @@
+// init syntax highlighter
+hljs.initHighlightingOnLoad();
+
 // top navigation
 $('#bs-example-navbar-collapse-1 li').click(function(e) {
-    $('#bs-example-navbar-collapse-1 li.active').removeClass('active');
-    var $this = $(this);
-    if (!$this.hasClass('active')) {
-        $this.addClass('active');
-    }
-    e.preventDefault();
-    hidePages(this.id)
+	$('#bs-example-navbar-collapse-1 li.active').removeClass('active');
+	var $this = $(this);
+	if (!$this.hasClass('active')) {
+		$this.addClass('active');
+	}
+	e.preventDefault();
+	hidePages(this.id)
 });
 
 $('#a-cargo').click(function(e) {
-    $('#bs-example-navbar-collapse-1 li.active').removeClass('active');
-    e.preventDefault();
-    hidePages(this.id)
+	$('#bs-example-navbar-collapse-1 li.active').removeClass('active');
+	e.preventDefault();
+	hidePages(this.id)
 });
 
 function hidePages(exceptId){
@@ -22,14 +25,16 @@ function hidePages(exceptId){
 
 // tutorials navigation
 $(".tutorials-link").click(function() {
-    $('html, body').animate({
-        scrollTop: $('#tutorialsSection-' + this.id.split('-')[1]).offset().top
-    }, 500);
+	$('html, body').animate({
+		scrollTop: $('#tutorialsSection-' + this.id.split('-')[1]).offset().top
+	}, 500);
 });
 
 // main
 main()
 
 function main() {
-     
+	// TODO delete this
+	$('#li-tutorials').click()
+
 }
