92ad0b2258c1188744a21fbab223ee3cb578261c
davecourtois <dave.courtois60@gmail.com>
2017-10-14 18:40:28 -0400

    Adjust entity prototype on the server side.

--- WebApp/Cargo/Script/Cargo/entityPrototype.js
+++ WebApp/Cargo/Script/Cargo/entityPrototype.js
@@ -8,7 +8,7 @@
  * @constructor 
  */
 var EntityPrototype = function () {
-    this.TYPENAME = "Server.EntityPrototype"
+      this.TYPENAME = "Server.EntityPrototype"
 
     /**
      * @property {string} TypeName The type name of the entity must be write 'packageName.className'
@@ -85,9 +85,24 @@ var EntityPrototype = function () {
     this.FieldsNillable = [] // bool
 
     /**
-     * @property FieldsOrder The field order
+     * @property The fields default value. Use to initialyse a new object with a given value.
      */
-    this.FieldsOrder = [] // int
+    this.FieldsOrder = []
+
+    /**
+     * @property FieldsOrder The field order
+    */
+    this.FieldsDefaultValue = [] // int
+
+    /**
+     * @property Contain the list of fields to delete
+    */
+    this.FieldsToDelete = [] // int
+
+    /**
+     * @property Contain the list of fields to rename
+    */
+    this.FieldsToUpdate = [] // int
 
     return this
 }
@@ -97,7 +112,25 @@ var EntityPrototype = function () {
  */
 EntityPrototype.prototype.getTitles = function () {
     // The get title default function... can be overload.
-    return [this.Ids[1]] // The first index only...
+    var titles = []
+
+    // The ids
+    for (var i = 1; i < this.Ids.length; i++) {
+        var fieldIndex = this.getFieldIndex(this.Ids[i])
+        var field = this.Fields[fieldIndex]
+        if (this.FieldsVisibility[fieldIndex] == true) {
+            titles.push(this[field])
+        }
+    }
+    // The indexs
+    for (var i = 1; i < this.Indexs.length; i++) {
+        var fieldIndex = this.getFieldIndex(this.Indexs[i])
+        var field = this.Fields[fieldIndex]
+        if (this.FieldsVisibility[fieldIndex] == true) {
+            titles.push(this[field])
+        }
+    }
+    return titles
 }
 
 
@@ -111,11 +144,28 @@ EntityPrototype.prototype.init = function (object) {
         return
     }
 
+    if (object.FieldsNillable == null) {
+        object.FieldsNillable = []
+        for (var i = 0; i < object.Fields.length; i++) {
+            object.FieldsNillable.push(false)
+        }
+    }
+
+    if (object.FieldsDocumentation == null) {
+        object.FieldsDocumentation = []
+        for (var i = 0; i < object.Fields.length; i++) {
+            object.FieldsDocumentation.push("")
+        }
+    }
+
     // The type name.
     this.TypeName = object.TypeName
 
     // The package will be an object on the global scope.
     this.PackageName = object.TypeName.split(".")[0]
+    if (window[this.PackageName] == undefined) {
+        window[this.PackageName] = eval(this.PackageName + " = {}")
+    }
 
     // The type name.
     this.ClassName = object.TypeName.substring(object.TypeName.indexOf(".") + 1)
@@ -129,12 +179,25 @@ EntityPrototype.prototype.init = function (object) {
     // Now the fields.
     if (object.Fields != null && object.FieldsType != null && object.FieldsVisibility != null && object.FieldsOrder != null) {
 
+        if (!contains(object.Fields, "ParentLnk")) {
+            object.Fields.unshift("ParentLnk")
+            object.FieldsType.unshift("xs.string")
+            object.FieldsVisibility.unshift(false)
+            object.FieldsNillable.unshift(false)
+            object.FieldsDocumentation.unshift("Relation with it parent.")
+            object.FieldsOrder.push(object.FieldsOrder.length)
+            object.FieldsDefaultValue.unshift("")
+        }
+
         // Append parent uuid if none is define.
         if (!contains(object.Fields, "ParentUuid")) {
             object.Fields.unshift("ParentUuid")
             object.FieldsType.unshift("xs.string")
             object.FieldsVisibility.unshift(false)
+            object.FieldsNillable.unshift(false)
+            object.FieldsDocumentation.unshift("The parent object UUID")
             object.FieldsOrder.push(object.FieldsOrder.length)
+            object.FieldsDefaultValue.unshift("")
         }
 
         // Append the uuid if none is define.
@@ -144,11 +207,14 @@ EntityPrototype.prototype.init = function (object) {
             object.FieldsType.unshift("xs.string")
             object.FieldsVisibility.unshift(false)
             object.FieldsOrder.push(object.FieldsOrder.length)
+            object.FieldsNillable.unshift(false)
+            object.FieldsDocumentation.unshift("The object UUID")
+            object.FieldsDefaultValue.unshift("")
             object.Ids.unshift("UUID")
         }
 
         for (var i = 0; i < object.Fields.length; i++) {
-            this.appendField(object.Fields[i], object.FieldsType[i], object.FieldsVisibility[i], object.FieldsOrder[i])
+            this.appendField(object.Fields[i], object.FieldsType[i], object.FieldsVisibility[i], object.FieldsOrder[i], object.FieldsNillable[i], object.FieldsDocumentation[i], object.FieldsDefaultValue[i])
             if (object.Fields[i] == "UUID") {
                 if (!contains(this.Ids, "UUID")) {
                     this.Ids.unshift("UUID")
@@ -157,6 +223,10 @@ EntityPrototype.prototype.init = function (object) {
                 if (!contains(this.Indexs, "ParentUuid")) {
                     this.Indexs.unshift("ParentUuid")
                 }
+            } else if (object.Fields[i] == "ParentLnk") {
+                if (!contains(this.Indexs, "ParentLnk")) {
+                    this.Indexs.unshift("ParentLnk")
+                }
             }
         }
 
@@ -164,26 +234,36 @@ EntityPrototype.prototype.init = function (object) {
         console.log(object.TypeName + " has no fields!!!")
     }
 
-    // Now the restriction
-    this.Restrictions = object.Restrictions
-
     // If the object is abstract
     this.IsAbstract = object.IsAbstract
 
+    // Now the restriction
+    this.Restrictions = object.Restrictions
+
+
     // The list of substitution type.
-    this.SubstitutionGroup = object.SubstitutionGroup
+    if (object.SubstitutionGroup != undefined) {
+        this.SubstitutionGroup = object.SubstitutionGroup
+    }
 
     // The list of Supertype 
-    this.SuperTypeNames = object.SuperTypeNames
+    if (object.SuperTypeNames != undefined) {
+        this.SuperTypeNames = object.SuperTypeNames
+    }
 
     // if the type is a collection.
-    this.ListOf = object.ListOf
+    if (object.ListOf != undefined) {
+        this.ListOf = object.ListOf
+    }
+
+    if (object.FieldsDefaultValue != undefined) {
+        this.FieldsDefaultValue = object.FieldsDefaultValue
+    }
 
     // other standard fields.
-    this.appendField("childsUuid", "[]xs.string", false, this.Fields.length)
-    this.appendField("referenced", "[]Server.EntityRef", false, this.Fields.length)
+    this.appendField("childsUuid", "[]xs.string", false, this.Fields.length, false, "the array of child entities.", "[]")
+    this.appendField("referenced", "[]Server.EntityRef", false, this.Fields.length, false, "The field documentation.", "[]")
 
-	// The constructor is generated on the server.
 }
 
 /**
@@ -193,7 +273,7 @@ EntityPrototype.prototype.init = function (object) {
  * @param {boolean} isVisible True, if the field is visible.
  * @param {int} order The order the field will be return, usefull to display.
  */
-EntityPrototype.prototype.appendField = function (name, typeName, isVisible, order) {
+EntityPrototype.prototype.appendField = function (name, typeName, isVisible, order, isNillable, documentation, defaultValue) {
     // Set the field name.
     if (!contains(this.Fields, name)) {
         this.Fields.push(name)
@@ -205,6 +285,12 @@ EntityPrototype.prototype.appendField = function (name, typeName, isVisible, ord
         this.FieldsVisibility.push(isVisible)
         // And the order (index in the list of fields.)
         this.FieldsOrder.push(parseInt(order))
+
+        this.FieldsNillable.push(isNillable)
+
+        this.FieldsDocumentation.push(documentation)
+
+        this.FieldsDefaultValue.push(defaultValue)
     }
 }
 
@@ -244,4 +330,48 @@ EntityPrototype.prototype.getFieldIndex = function (field) {
         }
     }
     return -1
+}
+
+
+/**
+ * That class is use to manage entity prototype event.
+ */
+var EntityPrototypeManager = function () {
+    if (server == undefined) {
+        return
+    }
+    EventHub.call(this, PrototypeEvent)
+
+    return this
+}
+
+EntityPrototypeManager.prototype = new EventHub(null);
+EntityPrototypeManager.prototype.constructor = EntityPrototypeManager;
+
+/**
+ * Overload the onEvent function to set the new  prototype object on the local map
+ * before propagate the event to listener.
+ */
+EntityPrototypeManager.prototype.onEvent = function (evt) {
+    if (evt.code == NewPrototypeEvent || evt.code == UpdatePrototypeEvent) {
+        // Set the prototype.
+        var prototype = new EntityPrototype()
+        prototype.init(evt.dataMap.prototype)
+        setEntityPrototype(prototype)
+        evt.dataMap.prototype = prototype
+    } else if (evt.code == DeletePrototypeEvent) {
+        // Remove it from the map
+        delete entityPrototypes[evt.dataMap.prototype.TypeName]
+    }
+
+    // Call the regular function.
+    EventHub.prototype.onEvent.call(this, evt);
+}
+
+/**
+ * Use that function to set value of the global map.
+ * @param {*} prototype 
+ */
+function setEntityPrototype(prototype) {
+    entityPrototypes[prototype.TypeName] = prototype
 }
