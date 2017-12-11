149cec88a58603e3da2b2c97893ea8b027b018aa
Dave <dave.courtois60@gmail.com>
2017-12-11 18:59:57 -0500

    Implementation of the event listener in the Cargo Entity Container. And I'm done with it!

---
+++ CargoServiceContainer/event.hpp
@@ -0,0 +1,56 @@
+#ifndef EVENT_HPP
+#define EVENT_HPP
+#include <QString>
+
+const QString AccountEvent = "AccountEvent";
+/*AccountRegisterSuccessEvent    = 0
+AccountConfirmationSucessEvent = 1
+SessionEvent                   = "SessionEvent"
+LoginEvent                     = 4
+LogoutEvent                    = 5
+StateChangeEvent               = 6
+BpmnEvent                      = "BpmnEvent"
+NewProcessInstanceEvent        = 7
+UpdateProcessInstanceEvent     = 8
+NewDefinitionsEvent            = 9
+DeleteDefinitionsEvent         = 10
+UpdateDefinitionsEvent         = 11
+EntityEvent                    = "EntityEvent"
+NewEntityEvent                 = 12
+UpdateEntityEvent              = 13
+DeleteEntityEvent              = 14
+OpenEntityEvent                = 15
+CloseEntityEvent               = 16
+FileEvent                      = "FileEvent"
+NewFileEvent                   = 17
+DeleteFileEvent                = 18
+UpdateFileEvent                = 19
+OpenFileEvent                  = 20
+CloseFileEvent                 = 21
+DataEvent                      = "DataEvent"
+DeleteRowEvent                 = 22
+NewRowEvent                    = 23
+UpdateRowEvent                 = 24
+NewDataStoreEvent              = 25
+DeleteDataStoreEvent           = 26
+SecurityEvent                  = "SecurityEvent"
+NewRoleEvent                   = 27
+DeleteRoleEvent                = 28
+UpdateRoleEvent                = 29
+PrototypeEvent                 = "PrototypeEvent"
+NewPrototypeEvent              = 30
+UpdatePrototypeEvent           = 31
+DeletePrototypeEvent           = 32
+ProjectEvent                   = "ProjectEvent"
+EmailEvent                     = "EmailEvent"
+ServiceEvent                   = "ServiceEvent"
+ConfigurationEvent             = "ConfigurationEvent"
+NewTaskEvent                   = 33
+UpdateTaskEvent                = 34
+EventEvent                     = "EventEvent"
+LdapEvent                      = "LdapEvent"
+OAuth2Event                    = "OAuth2Event"
+SchemaEvent                    = "SchemaEvent"
+WorkflowEvent                  = "WorkflowEvent"*/
+
+#endif // EVENT_HPP
