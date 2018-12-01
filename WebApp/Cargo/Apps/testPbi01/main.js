94af0fb53afb83d8a78458c52c9297953b992755
davecourtois <dave.courtois60@gmail.com>
2018-12-01 13:42:50 -0500

    remove test project

--- WebApp/Cargo/Apps/testPbi01/main.js
+++
@@ -1,7 +0,0 @@
-// Load the interface.
-function main(){
-    var div = document.createElement("div");
-    document.getElementsByTagName("body")[0].appendChild(div);
-    var toto = new powerbi.extensibility.visual.Visual({"element":div});
-    console.log("---> Im loaded!", toto);
-}
